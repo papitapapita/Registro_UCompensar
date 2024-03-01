d587ad297fad15b793b33ca888f32d1f7794b29e
Jonathan <many.d.r.o.2001@hotmail.com>
2024-03-01 12:52:31 -0500

    First version of the project

---
+++ src/main/java/com/mycompany/registro_ucompensar/Registro_UCompensar.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registro_ucompensar;
+
+/**
+ *
+ * @author manyd
+ */
+public class Registro_UCompensar {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
